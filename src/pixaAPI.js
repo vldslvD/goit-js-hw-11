db0c7c268116664f4a3a5ee69d5ac643fcdb033c
vldslvD <dobritsavlad@gmail.com>
2023-06-19 02:06:08 +0200

    console.log is deleted

--- src/pixaAPI.js
+++ src/pixaAPI.js
@@ -14,6 +14,6 @@ export async function getPictures(query, pageCount) {
   };
   query = query.replace(/ /g, '+');
   const response = await axios.get(`${BASE_URL}?q=${query}`, options);
-  console.log(response);
+  
   return response;
 }
